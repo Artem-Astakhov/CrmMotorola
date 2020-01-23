ac7a8e9db658bed25698d7db1687fd0d2a3b31cd
Artem <Astakhov.Artem91@gmail.com>
2020-01-23 23:51:21 +0200

    Создание Базы

---
+++ CrmBL/Model/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBL.Model
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Prise { get; set; }
+        public int Count { get; set; }
+        public string Color { get; set; }
+
+        public virtual ICollection<Sell> Sells { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
